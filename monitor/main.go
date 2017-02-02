eeddbb4b86ca2cf8649069a6e231db8f9202c113
domac <quanix@163.com>
2017-02-02 17:58:04 +0800

    add monitor example

---
+++ monitor/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+
+	"github.com/rcrowley/go-metrics"
+	"github.com/vrischmann/go-metrics-influxdb"
+)
+
+//使用内存
+func useMemory() {
+	a := rand.Intn(100)
+	m := make([]int, a*1e6)
+
+	d := time.Second * time.Duration(rand.Intn(15))
+	fmt.Printf("Generated something with size %dMB, sleeping for %s\n", len(m)/1e6, d)
+	time.Sleep(d)
+	fmt.Printf("Done using %dMB\n", len(m)/1e6)
+}
+
+func main() {
+
+	r := metrics.NewRegistry()
+	metrics.RegisterDebugGCStats(r)
+	metrics.RegisterRuntimeMemStats(r)
+
+	go metrics.CaptureDebugGCStats(r, time.Second*5)
+	go metrics.CaptureRuntimeMemStats(r, time.Second*5)
+
+	go influxdb.InfluxDB(
+		r,                             // metrics registry
+		time.Second*5,                 // 时间间隔
+		"http://192.168.139.140:8086", // InfluxDB url
+		"helloWorld",                  // InfluxDB 数据库名
+		"",                            // InfluxDB user
+		"",                            // InfluxDB password
+	)
+
+	time.Sleep(time.Second * 10)
+
+	for {
+		go useMemory()
+		time.Sleep(time.Duration(rand.Intn(15)) * time.Second)
+	}
+}
